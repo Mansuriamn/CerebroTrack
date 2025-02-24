24a1476373d4a2c4741becb8248e7a0bb29ea837
Aman Mansuri <162823789+Mansuriamn@users.noreply.github.com>
2025-02-24 10:49:00 +0530

    hello

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -6,28 +6,17 @@ export default function Footer() {
     
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
    <section   id="contact">
-    <div className="footer-waves">
-      <div className="wave" id="wave1" />
-      <div className="wave" id="wave2" />
-      <div className="wave" id="wave3" />
-      <div className="wave" id="wave4" />
-    </div>
+   
 
 
 
     <div className="footer-content">
       <div className="footer-section">
         <h3 id='lets'>Let's Connect</h3>
-        {/* <div className="home-Sci">
-          <a href="https://www.instagram.com/coding419" target="_blank" style={{"--i":"7"}}><i className="bx bxl-instagram-alt" /></a>
-          <a href="https://wa.me/+919832023985" target="_blank" style={{"--i":"8"}}><i className="bx bxl-whatsapp" /></a>
-          <a href="https://www.linkedin.com/in/shekh-dalim-941884275/" target="_blank" style={{"--i":"9"}}><i className="bx bxl-linkedin-square" /></a>
-          
-          <a href="https://github.com/Shekh-Dalim" target="_blank" style={{"--i":"10"}}><i className="bx bxl-github" /></a>
-        </div> */}
+      
       </div>
       <div className="footer-section">
-        <h3>Quick Links</h3>
+        <h3  id='lets'>Quick Links</h3>
         <ul className="footer-links">
           <li><a href="#">Home</a></li>
           <li><a href="#about">About</a></li>
@@ -37,8 +26,8 @@ export default function Footer() {
         </ul>
       </div>
       <div className="footer-section">
-        <h3>Contact Info</h3>
-        <p><i className="bx bxs-envelope" /> mansooreeaman@gmail.com</p>
+        <h3 id='lets'>Contact Info</h3>
+        <p><i  className="bx bxs-envelope" /> mansooreeaman@gmail.com</p>
         <p><i className="bx bxs-phone" /> 9302877751</p>
       </div>
       {/* Centered Copyright Section */}
